bc8d56cc1c483c49b5aaf01ec4acd9d3262dac37
yen Lin <erte00000@gmail.com>
2023-08-27 14:57:52 +0800

    final change

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-// const API_URL = "https://courseweb-backend.onrender.com/api/user";
-const API_URL = "http://localhost:8080/api/user";
+const API_URL = "https://courseweb-backend.onrender.com/api/user";
+// const API_URL = "http://localhost:8080/api/user";
 
 class AuthService {
   register(username, email, password, role) {
--- src/services/course.service.js
+++ src/services/course.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-// const API_URL = "https://courseweb-backend.onrender.com/api/course";
-const API_URL = "http://localhost:8080/api/course/";
+const API_URL = "https://courseweb-backend.onrender.com/api/course";
+// const API_URL = "http://localhost:8080/api/course/";
 
 function getToken() {
   let token;
