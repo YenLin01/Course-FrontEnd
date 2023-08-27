6025559186e3b8e37063511152b94f1d3cb6797c
yen Lin <erte00000@gmail.com>
2023-08-27 20:38:03 +0800

    content update

--- src/services/course.service.js
+++ src/services/course.service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "https://courseweb-backend.onrender.com/api/course";
+const API_URL = "https://courseweb-backend.onrender.com/api/course/";
 // const API_URL = "http://localhost:8080/api/course/";
 
 function getToken() {
